3ce179389ef21036347383e3fe814adf9711cd4d
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-05-09 20:29:55 -0700

    Support multiple actions in one run
    For example, "./koality.py package upgrade" to do a full upgrade

--- koality.py
+++ koality.py
@@ -2,23 +2,45 @@
 
 import sys
 
+from collections import OrderedDict
+
 
 def main():
-	mode = sys.argv[1] if len(sys.argv) > 1 else None
-	if mode == 'run':
-		from manager.run import Runner
-		Runner().run()
-	elif mode == 'install':
-		from manager.install import Installer
-		Installer().run()
-	elif mode == 'upgrade':
-		from manager.upgrade import Upgrader
-		Upgrader().run()
-	elif mode == 'package':
+	def package():
 		from manager.package import Packager
 		Packager().run()
-	else:
-		raise Exception('No valid mode specified. Must be "run", "install", or "upgrade"')
+
+	def install():
+		from manager.install import Installer
+		Installer().run()
+
+	def upgrade():
+		from manager.upgrade import Upgrader
+		Upgrader().run()
+
+	def run():
+		from manager.run import Runner
+		Runner().run()
+
+	valid_actions = OrderedDict([
+		('package', package),
+		('install', install),
+		('upgrade', upgrade),
+		('run', run)
+	])
+
+	chosen_actions = sys.argv[1:]
+
+	if not chosen_actions:
+		raise Exception('No valid actions specified. Must be in %s' % valid_actions.keys())
+
+	invalid_actions = filter(lambda action: action not in valid_actions, chosen_actions)
+	if invalid_actions:
+		raise Exception('Invalid actions %s specified. Must be in %s' % (invalid_actions, valid_actions.keys()))
+
+	for action_name, action_method in valid_actions.iteritems():
+		if action_name in chosen_actions:
+			action_method()
 
 
 if __name__ == '__main__':
