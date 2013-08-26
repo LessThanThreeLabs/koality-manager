35791a86c0e6261394a4eee04cacfbd1e664a569
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-08-26 15:03:22 -0700

    Install jgit from branch 0.3

--- manager/package/scripts/jgit.py
+++ manager/package/scripts/jgit.py
@@ -7,15 +7,16 @@ from manager.shared.script import ShellScript
 class JgitPackageScript(ShellScript):
 	@classmethod
 	def get_script(cls):
+		jgit_branch = '0.3'
 		jgit_dir = os.path.join(dependencies_directory, 'jgit')
 		jgit_version_file = os.path.join(jgit_dir, '.version')
 		return '''
-			headsha=$(git ls-remote git://github.com/LessThanThreeLabs/jgit.git refs/heads/master | awk '{print $1}')
+			headsha=$(git ls-remote git://github.com/LessThanThreeLabs/jgit.git refs/heads/%s | awk '{print $1}')
 			if [ -f %s ] && [ "$(cat %s)" == "$headsha" ]; then
 				echo "Newest jgit already packaged"
 			else
 				cd /tmp
-				git clone git://github.com/LessThanThreeLabs/jgit.git
+				git clone git://github.com/LessThanThreeLabs/jgit.git -b %s
 				cd jgit
 				sudo apt-get install -y maven
 				mvn install -Dmaven.test.skip=true || exit 1
@@ -24,8 +25,10 @@ class JgitPackageScript(ShellScript):
 				echo $headsha > %s
 				rm -rf /tmp/jgit
 			fi
-		''' % (jgit_version_file,
+		''' % (jgit_branch,
 				jgit_version_file,
+				jgit_version_file,
+				jgit_branch,
 				jgit_dir,
 				jgit_dir,
 				jgit_version_file)
