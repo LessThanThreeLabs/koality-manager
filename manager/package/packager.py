815116b2237344a1dbdcd4f5abdd1a89a75b4dbc
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-09-25 10:26:17 -0700

    0.4.6 Stable(?)

--- manager/package/packager.py
+++ manager/package/packager.py
@@ -6,7 +6,7 @@ from scripts import package_scripts
 
 
 class Packager(object):
-	version = '0.4.6-internal-1'
+	version = '0.4.6'
 	packaged_directory = os.path.join('/tmp', 'koality', version)
 	internal_packaged_directory = os.path.abspath(os.path.join(packaged_directory, 'koality'))
 
