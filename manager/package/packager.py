22223f2bf3d7bb9580987931b76aeb45fc4c9391
Brian Bland <r4nd0m1n4t0r@gmail.com>
2013-10-02 16:09:08 -0700

    Bump to 0.6.1 with 0.6.1 agles

--- manager/package/packager.py
+++ manager/package/packager.py
@@ -6,7 +6,7 @@ from scripts import package_scripts
 
 
 class Packager(object):
-	version = '0.6.0'
+	version = '0.6.1-internal-1'
 	packaged_directory = os.path.join('/tmp', 'koality', version)
 	internal_packaged_directory = os.path.abspath(os.path.join(packaged_directory, 'koality'))
 
